1ff3fb547e25852a73128d3bf1652274c5996cab
diaszakir <mailforstudy84@gmail.com>
2025-01-14 15:54:21 +0500

    variables

--- 2Variables.sol
+++ 2Variables.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract Variables{
+  address userAddress = 0x9Af0A55E3365696D6AbAEe81fD65d1f1356eE923;
+  string message = "Hello, Solidity!";
+  bool isReady = true;
+  uint8 MAX_SUPPLY = 10;
+  uint256 WAIT_TIME = 1 days;
+}
